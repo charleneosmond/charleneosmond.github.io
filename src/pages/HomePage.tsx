c2a5fc0645df7adcd08862cd4e073d15e8059fd4
Clarice Osmond <clariceosmond@Clarices-MacBook-Pro.local>
2022-05-29 12:39:28 -0400

    Logo, Phone fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,38 @@
 import * as React from "react";
-import HPex from "../images/HPex.jpeg";
-import { makeStyles, Typography, Grid } from "@material-ui/core";
+import HPweb from "../images/HPweb.jpg";
+import HPmb from "../images/HPmb.jpg";
+import {
+  makeStyles,
+  Typography,
+  Grid,
+  useTheme,
+  useMediaQuery,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   hpContainer: {
     width: "100vw",
-    height: "100vh",
-    // border: "3px solid red",
+    [theme.breakpoints.up("sm")]: {
+      height: "100vh",
+    },
+    //border: "3px solid red",
     justifyContent: "center",
     alignItems: "center",
     display: "flex",
     fontFamily: "Spectral Sc",
+    overflow: "hidden",
   },
   bgImage: {
     // border: "3px solid yellow",
-    maxWidth: "100%",
-    maxHeight: "100%",
+    width: "100vw",
   },
   hpGrid: {
     //border: "3px solid green",
     fontSize: "8px",
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up("sm")]: {
       fontSize: "16px",
     },
-    [theme.breakpoints.up("lg")]: {
+    [theme.breakpoints.up("md")]: {
       fontSize: "25px",
     },
     [theme.breakpoints.up("xl")]: {
@@ -37,23 +46,43 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     fontSize: "3em",
     fontFamily: "Spectral Sc",
+    color: "white",
+    textShadow: "5px 5px 5px black",
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2em",
+    },
   },
   hpButton: {
     marginTop: "20px",
     fontSize: "1.5em",
+    textShadow: "5px 5px 5px black",
     padding: "0.5em 1.25em",
     [theme.breakpoints.up("sm")]: {
+      fontSize: "0.8em",
+      padding: "1em 2em",
+    },
+    [theme.breakpoints.up("md")]: {
       fontSize: "0.5em",
       padding: "1em 2em",
     },
-    backgroundColor: "rgba(255, 255, 255, 0.61)",
+    [theme.breakpoints.up("xl")]: {
+      fontSize: "0.4em",
+      padding: "0.75em 1.5em",
+    },
+    backgroundColor: "transparent",
+    color: "white",
+    border: "1px solid white",
+    boxShadow: "2px 2px black",
   },
 }));
-//<img src={HPex} className={classes.bgImage} />
+
 const HomePage: React.FC = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const bgImage = useMediaQuery(theme.breakpoints.up("sm")) ? HPweb : HPmb;
   return (
     <div className={classes.hpContainer}>
+      <img src={bgImage} className={classes.bgImage} />
       <Grid container direction="column" className={classes.hpGrid}>
         <Typography className={classes.hpName}> Charlene Osmond </Typography>
         <button className={classes.hpButton}> ENTER </button>{" "}
